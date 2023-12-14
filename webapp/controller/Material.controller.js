aebb24d4492c09818a2ee5c0d143f379db39cc8e
Senne2002 <senne.knaepkens@telenet.be>
2023-12-14 12:08:47 +0100

    Add basic filtering

--- webapp/controller/Material.controller.js
+++ webapp/controller/Material.controller.js
@@ -1,15 +1,65 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/Device",
+    "sap/ui/model/Filter",
+    "sap/ui/core/Fragment",
+  ],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, Device, Filter, Fragment) {
+    "use strict";
 
-        return Controller.extend("ap.materialsales.controller.Material", {
-            onInit: function () {
+    return Controller.extend("ap.materialsales.controller.Material", {
+      onInit: function () {
+        this._mViewSettingsDialogs = {};
+      },
 
-            }
+      //filtering
+      handleFilterButtonPressed: function () {
+        this.getViewSettingsDialog(
+          "ap.materialsales.fragments.materialFilter"
+        ).then(function (oViewSettingsDialog) {
+          oViewSettingsDialog.open();
         });
+      },
+      handleFilterDialogConfirm: function (oEvent) {
+        var oTable = this.byId("materialTable"),
+          mParams = oEvent.getParameters(),
+          oBinding = oTable.getBinding("items"),
+          aFilters = [];
+
+        mParams.filterItems.forEach(function (oItem) {
+          let sPath = oItem.getParent().getKey(),
+            sOperator = "EQ",
+            sValue1 = oItem.getKey(),
+            oFilter = new Filter(sPath, sOperator, sValue1);
+
+          aFilters.push(oFilter);
+        });
+
+        // apply filter settings
+        oBinding.filter(aFilters);
+      },
+      getViewSettingsDialog: function (sDialogFragmentName) {
+        var pDialog = this._mViewSettingsDialogs[sDialogFragmentName];
+
+        if (!pDialog) {
+          pDialog = Fragment.load({
+            id: this.getView().getId(),
+            name: sDialogFragmentName,
+            controller: this,
+          }).then(function (oDialog) {
+            if (Device.system.desktop) {
+              oDialog.addStyleClass("sapUiSizeCompact");
+            }
+            return oDialog;
+          });
+          this._mViewSettingsDialogs[sDialogFragmentName] = pDialog;
+        }
+        return pDialog;
+      },
     });
+  }
+);
